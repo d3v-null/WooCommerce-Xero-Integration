4fe966ba54558757c4a1f0e37eddfeffc10acf26
derwentx <derwentx@gmail.com>
2017-06-19 17:31:28 +1000

    better container classes, better debug info

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 from os import sys, path
-import yaml
+import csv
 from copy import copy
 from collections import OrderedDict
+from time import sleep
 # from woocommerce import API as WCAPI
 from pprint import pprint
+from requests import ReadTimeout
+
+import yaml
 from tabulate import tabulate
 import argparse
 
@@ -12,7 +16,7 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from src.utils import SanitationUtils, DebugUtils
 from src.api_clients import WcClient, XeroClient, WpClient
-from src.containers import Xero_API_Product, WC_API_Product
+from src.containers import XeroProduct, WCProduct, WCAPIProduct, WCCSVProduct
 
 def main():
     dir_module = os.path.dirname(sys.argv[0])
@@ -20,21 +24,16 @@ def main():
         os.chdir(dir_module)
 
     #default arguments
-
-    download_wc = False
-    download_xero = False
-    download_wc = True
-    download_xero = True
-    update_wc = False
-    update_wc = True
     xero_config_file = 'xero_api.yaml'
     wc_config_file = 'wc_api_test.yaml'
-    report_and_quit = False
+    xero_report_file = 'xero_products.csv'
+    wc_report_file = 'wc_products.csv'
+    new_wc_report_file = 'new_wc_products.csv'
 
-    parser = argparse.ArgumentParser(description = 'Merge products between Xero and WC')
+    parser = argparse.ArgumentParser(description='Merge products between Xero and WC')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbosity", action="count",
-                        help="increase output verbosity")
+    group.add_argument("-v", "--verbosity", action="count", default=2,
+                       help="increase output verbosity")
     group.add_argument("-q", "--quiet", action="store_true")
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--download-xero', help='download the xero data',
@@ -46,10 +45,14 @@ def main():
                        action="store_true", default=None)
     group.add_argument('--skip-download-wc', help='use the local wc file instead\
         of downloading the wc data', action="store_false", dest='download_wc')
+    parser.add_argument('--wc-import-file',
+                        help='location of wc import file, used if can\'t download products')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--update-wc', help='update the wc database',
                        action="store_true", default=None)
-    group.add_argument('--report-and-quit', help='report wc data and quit',
+    group.add_argument('--report-wc-and-quit', help='report wc data and quit',
+                       action="store_true", default=None)
+    group.add_argument('--report-xero-and-quit', help='report wc data and quit',
                        action="store_true", default=None)
     group.add_argument('--skip-update-wc', help='don\'t update the wc database',
                        action="store_false", dest='update_wc')
@@ -62,18 +65,10 @@ def main():
             DebugUtils.CURRENT_VERBOSITY = args.verbosity
         elif args.quiet:
             DebugUtils.CURRENT_VERBOSITY = 0
-        if args.download_wc is not None:
-            download_wc = args.download_wc
-        if args.download_xero is not None:
-            download_xero = args.download_xero
-        if args.update_wc is not None:
-            update_wc = args.update_wc
         if args.xero_config_file is not None:
             xero_config_file = args.xero_config_file
         if args.wc_config_file is not None:
             wc_config_file = args.wc_config_file
-        if args.report_and_quit is not None:
-            report_and_quit = args.report_and_quit
 
     dir_conf = 'conf'
     path_conf_wc = os.path.join(dir_conf, wc_config_file)
@@ -88,74 +83,113 @@ def main():
         for key in ['consumer_key', 'consumer_secret', 'key_file']:
             assert key in conf_xero, key
 
-    wcClient = WpClient(
+    wc_client = WpClient(
         **conf_wc
     )
 
-    xeroClient = XeroClient(
+    xero_client = XeroClient(
         **conf_xero
     )
 
-    if download_wc:
-        wc_products = wcClient.get_products({
+    # fill wc_products
+
+    wc_products = []
+    wc_container_class = WCAPIProduct
+    if args.download_wc:
+        wc_products_data = wc_client.get_products({
             'core_fields': [
-                WC_API_Product.id_key,
-                WC_API_Product.managing_stock_key,
-                WC_API_Product.stock_level_key,
-                WC_API_Product.stock_status_key,
-                WC_API_Product.second_sku_key,
-                WC_API_Product.title_key,
+                wc_container_class.id_key,
+                wc_container_class.managing_stock_key,
+                wc_container_class.stock_level_key,
+                wc_container_class.stock_status_key,
+                wc_container_class.second_sku_key,
+                wc_container_class.title_key,
                 # 'status',
             ],
             'meta_fields': ['MYOB SKU'],
-            'per_page': 300
+            'per_page': conf_wc.get('per_page', 300)
         })
-    else:
-        wc_products = [
-            {'id': 1, 'meta.MYOB SKU':'a', 'title': 'A', 'stock_quantity': 1, 'managing_stock': True},
-            {'id': 2, 'sku':'a', 'title':'B', 'stock_quantity': 2, 'managing_stock': True},
-            {'id': 3, 'sku':'c', 'title':'C', 'stock_quantity': 3, 'managing_stock': True}]
+        wc_products = [wc_container_class(data) for data in wc_products_data]
 
-    print "wc products:", len(wc_products)
-    pprint(wc_products)
-    tabulate(wc_products)
+        if not wc_products:
+            print "it looks like there was a problem downloading products from the website."
+            print "you can export the products from the website manually, and try again."
+            quit()
 
-    if report_and_quit:
+    elif args.wc_import_file:
+        wc_container_class = WCCSVProduct
+        with open(args.wc_import_file, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                wc_products.append(wc_container_class(row))
+
+    if wc_products:
+        DebugUtils.register_message("reporting WC")
+
+        with open(wc_report_file, 'w+') as report_file:
+            csv_writer = csv.writer(report_file)
+            csv_writer.writerow(wc_container_class.report_attrs)
+            for wc_product in wc_products:
+                csv_writer.writerow([
+                    SanitationUtils.coerce_ascii(getattr(wc_product, attr)) \
+                    for attr in wc_container_class.report_attrs
+                ])
+
         for wc_product in wc_products:
-            wc_sku = wc_product.get(WC_API_Product.sku_key)
-            wc_second_sku = wc_product.get(WC_API_Product.second_sku_key)
-            wc_managing_stock = wc_product.get(WC_API_Product.managing_stock_key)
-            wc_stock_level = wc_product.get(WC_API_Product.stock_level_key)
+            wc_sku = wc_product.get(wc_container_class.sku_key)
+            wc_second_sku = wc_product.get(wc_container_class.second_sku_key)
+            wc_managing_stock = wc_product.get(wc_container_class.managing_stock_key)
+            wc_stock_level = wc_product.get(wc_container_class.stock_level_key)
             if not wc_managing_stock and wc_stock_level < 5:
-                print 'not tracked:', wc_sku
-            if wc_sku != wc_second_sku:
-                print 'WC SKU is not the same as second WC SKU:', wc_sku, wc_second_sku
+                DebugUtils.register_warning('not tracked: %s' % wc_sku)
+            if wc_sku and wc_sku != wc_second_sku:
+                DebugUtils.register_warning('primary SKU (%s) != secondary SKU (%s) ' % (wc_sku, wc_second_sku))
+
+    if args.report_wc_and_quit:
         quit()
 
-    if download_xero:
-        xero_products = xeroClient.get_products()
+    if args.download_xero:
+        xero_products_data = xero_client.get_products()
+        xero_products = [XeroProduct(data) for data in xero_products_data]
+
     else:
-        xero_products = [
+        xero_products_data = [
             {'Code':'a', 'Name':'A', 'QuantityOnHand':2.0, 'IsTrackedAsInventory':True},
             {'Code':'c', 'QuantityOnHand':0.0, 'IsTrackedAsInventory':True}
         ]
+        xero_products = [XeroProduct(data) for data in xero_products_data]
 
-    DebugUtils.register_message( "xero_products: %s" % (len(xero_products)) )
+
+    if xero_products:
+        DebugUtils.register_message("reporting Xero")
+
+        with open(xero_report_file, 'w+') as report_file:
+            csv_writer = csv.writer(report_file)
+            csv_writer.writerow(XeroProduct.report_attrs)
+            for xero_product in xero_products:
+                csv_writer.writerow([
+                    SanitationUtils.coerce_ascii(getattr(xero_product, attr)) \
+                    for attr in XeroProduct.report_attrs
+                ])
+
+    if args.report_xero_and_quit:
+        quit()
+
+    DebugUtils.register_message("xero_products: %s" % (len(xero_products)))
 
     #group xero products by sku
     xero_sku_groups = {}
-    for xero_product_data in xero_products:
-        xero_product = Xero_API_Product(xero_product_data)
+    for xero_product in xero_products:
         xero_name = xero_product.title
         xero_sku = xero_product.sku
         xero_stock = xero_product.stock_level
         xero_id = xero_product.pid
-        xero_managing_stock = xero_product.managing_stock
+        # xero_managing_stock = xero_product.managing_stock
 
-        DebugUtils.register_message( " ".join(
+        DebugUtils.register_message(" ".join(
             SanitationUtils.coerce_ascii(x) for x in \
-            [ "processing xero", xero_id, xero_name, xero_sku, xero_stock]
-        ) )
+            ["processing xero", xero_id, xero_name, xero_sku, xero_stock]
+        ))
 
         if xero_sku:
             xero_sku_groups[xero_sku] = [
@@ -166,16 +200,15 @@ def main():
 
     #group wc products by sku
     wc_sku_groups = {}
-    for wc_product_data in wc_products:
-        wc_product = WC_API_Product(wc_product_data)
+    for wc_product in wc_products:
         wc_name = wc_product.title
         wc_sku = wc_product.sku
         wc_stock = wc_product.stock_level
         wc_id = wc_product.pid
-        DebugUtils.register_message( " ".join(
+        DebugUtils.register_message(" ".join(
             SanitationUtils.coerce_ascii(x) for x in \
             ["processing wc", wc_id, wc_name, wc_sku, wc_stock]
-        ) )
+        ))
         if wc_sku:
             wc_sku_groups[wc_sku] = [
                 prod for prod in \
@@ -192,7 +225,7 @@ def main():
 
     good_matches = []
     bad_matches = []
-    skip_matches = []
+    # skip_matches = []
     bad_data = []
     delta_matches = []
     delta_data = []
@@ -242,12 +275,17 @@ def main():
     for delta_match in delta_matches:
         wc_product = delta_match[0][0]
         xero_product = delta_match[1][0]
-        wc_name = wc_product.title
-        xero_name = xero_product.title
+        # wc_name = wc_product.title
+        # xero_name = xero_product.title
+        wc_level = wc_product.stock_level
+        xero_level = xero_product.stock_level
         delta_row = [
             SanitationUtils.coerce_ascii(cell) for cell in\
-            [wc_product, wc_product.stock_level, wc_product.managing_stock, \
-             xero_product, xero_product.stock_level, xero_product.managing_stock]
+            [
+                wc_product, wc_level, wc_product.managing_stock,
+                xero_product, xero_level, xero_product.managing_stock,
+                "%+3d" % (xero_level - wc_level)
+            ]
         ]
         delta_data.append(delta_row)
 
@@ -267,37 +305,64 @@ def main():
         #     ["changing", wc_name, xero_name, wc_stock, xero_stock]))
         # )
 
-    print "good", len(good_matches)
-    print "bad", len(bad_matches)
-    print "delta", len(delta_matches)
+    DebugUtils.register_message("good:  %3d" % len(good_matches))
+    DebugUtils.register_message("bad:   %3d" % len(bad_matches))
+    DebugUtils.register_message("delta: %3d" % len(delta_matches))
 
     # report bad
 
-    print "CONFLICTS:"
-
-    print tabulate(bad_data)
+    if bad_data:
+        DebugUtils.register_message("CONFLICTS:\n%s" % tabulate(
+            bad_data,
+            headers=('WC Products', 'Xero Products')
+        ))
 
     # report deltas
 
-    print "CHANGES:"
+    if delta_matches:
+        DebugUtils.register_message("CHANGES:\n%s" % tabulate(
+            delta_data, headers=(
+                'WC Product', 'stock', 'managing',
+                'Xero Product', 'stock', 'managing', 'delta'
+            )
+        ))
 
-    print tabulate(delta_data)
+    if new_wc_products:
+        DebugUtils.register_message("reporting WC to %s" % new_wc_report_file)
 
-    print "NEW WORDPRESS RECORDS"
+        with open(new_wc_report_file, 'w+') as report_file:
+            csv_writer = csv.writer(report_file)
+            csv_writer.writerow(WCProduct.report_attrs)
+            for wc_product in new_wc_products:
+                csv_writer.writerow([
+                    SanitationUtils.coerce_ascii(getattr(wc_product, attr)) \
+                    for attr in WCProduct.report_attrs
+                ])
 
-    print tabulate(new_data)
-
-    if update_wc:
+    if args.update_wc:
         for product in new_wc_products:
             data = {"product": OrderedDict([
-                (WC_API_Product.stock_level_key, product.stock_level),
-                (WC_API_Product.stock_status_key, product.stock_status),
-                (WC_API_Product.managing_stock_key, product.managing_stock)
+                (WCAPIProduct.stock_level_key, product.stock_level),
+                (WCAPIProduct.stock_status_key, product.stock_status),
+                (WCAPIProduct.managing_stock_key, product.managing_stock)
             ])}
 
-            print wcClient.update_product(product.pid, data)
+            response = None
+            try:
+                response = wc_client.update_product(product.pid, data)
+            except ReadTimeout as e:
+                print "request timed out, trying again after a short break"
+                sleep(10)
+                try:
+                    response = wc_client.update_product(product.pid, data)
+                except ReadTimeout as e:
+                    print "!!! API keeps timing out, please try again later"
+                    quit()
+            finally:
+                DebugUtils.register_message("API responeded :%s" % response)
+
     else:
-        print "not updating"
+        DebugUtils.register_message("Not updating")
 
 if __name__ == '__main__':
     main()
